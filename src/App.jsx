9d9e9076520ab1d6a11bca2de26c833f88211551
bruceathy <67832593+bruceathy@users.noreply.github.com>
2025-07-15 00:52:33 -0400

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,13 @@
-import { useState } from "react";
+import MainSec from "./components/MainSec.jsx";
+import Header from "./components/Header.jsx";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="background-space">
+      <Header />
+      <MainSec />
+    </div>
   );
 }
 
---
+++ src/components/MainSec.jsx
@@ -0,0 +1,13 @@
+export default function Main() {
+  return (
+    <main>
+      <nav className="navbar">
+        <ul>
+          <li>About</li>
+          <li>Services</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </main>
+  );
+}
