920f2906a2fed063acfe81b1f366b2eebe87a880
bruceathy <67832593+bruceathy@users.noreply.github.com>
2025-07-18 10:36:33 -0400

    up

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -1,23 +1,23 @@
-import github from "../assets/github-logo.svg";
-import linkedin from "../assets/linkedin-logo.svg";
-import xlogo from "../assets/x-logo.svg";
+import github from "../assets/github.png";
+import linkedin from "../assets/linkedin.png";
+import xlogo from "../assets/x.png";
 
 export default function Socials() {
   return (
     <ul className="social-links">
       <li>
         <a href="https://github.com/BruceAthy">
-          <img src={github} alt="github" />
+          <img src={github} alt="github" className="social-logo" />
         </a>
       </li>
       <li>
         <a href="https://www.linkedin.com/in/bruce-athy/">
-          <img src={linkedin} alt="linkedin" />
+          <img src={linkedin} alt="linkedin" className="social-logo" />
         </a>
       </li>
       <li>
         <a href="https://twitter.com/BruceAthy">
-          <img src={xlogo} alt="x" />
+          <img src={xlogo} alt="x" className="social-logo" />
         </a>
       </li>
     </ul>
