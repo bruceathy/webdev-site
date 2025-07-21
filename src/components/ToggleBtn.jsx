3d855b74e67093098db94a0b8a0d19bd74f70414
bruceathy <67832593+bruceathy@users.noreply.github.com>
2025-07-21 01:13:19 -0400

    togglebtn

--- src/components/ToggleBtn.jsx
+++ src/components/ToggleBtn.jsx
@@ -4,5 +4,19 @@ import { useState } from "react";
 
 export default function ToggleBtn() {
   const [dark, setDark] = useState(false);
-  return <></>;
+
+  const handleToggle = () => {
+    setDark(!dark);
+    document.body.classList.toggle("dark");
+  };
+  return (
+    <label className="toggle-switch">
+      <input type="checkbox" checked={dark} onChange={handleToggle} />
+      <span className="slider round" />
+      <span className="labels">
+        <span>Light</span>
+        <span>Dark</span>
+      </span>
+    </label>
+  );
 }
