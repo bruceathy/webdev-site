9e05c8f8dfbf06b92853887d9cd7fbb0e9f3db9b
bruceathy <67832593+bruceathy@users.noreply.github.com>
2025-12-02 17:53:25 -0500

    up

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import App from "./App.jsx";
+import HomePage from "./pages/HomePage.jsx";
 
 const queryClient = new QueryClient();
 
@@ -10,7 +10,7 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <App />
+        <HomePage />
       </BrowserRouter>
     </QueryClientProvider>
   </StrictMode>
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,18 @@
+import Header from "../components/Header";
+import Navbar from "../components/Navbar";
+import MainSec from "../components/MainSec";
+import "../css/app.css";
+import "../css/mobile.css";
+import "../css/tablet.css";
+
+function App() {
+  return (
+    <main>
+      <Header />
+      <Navbar />
+      <MainSec />
+    </main>
+  );
+}
+
+export default App;
