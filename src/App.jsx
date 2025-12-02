0b8aa77d2421d33c02d7c0d89d4e94e364ed6c65
bruceathy <67832593+bruceathy@users.noreply.github.com>
2025-12-02 17:39:50 -0500

    pages / responsive

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import MainSec from "./components/MainSec.jsx";
 import Header from "./components/Header.jsx";
 import Navbar from "./components/Navbar.jsx";
 import "./App.css";
+import "./css/mobile.css";
+import "./css/tablet.css";
 
 function App() {
   return (
